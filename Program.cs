82366394bdaa996b37219b60c26505e773b2cba2
Irina Nazarenko <irkaa.911.21@gmail.com>
2023-10-05 23:22:05 +0500

    add numbers2

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+System.Console.WriteLine("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    System.Console.WriteLine($"Number --> {num1} <-- is greater!");
+    System.Console.WriteLine($"Number --> {num2} <-- is smaller!");
+}
+else
+{
+    if (num2 > num1)
+    {
+        System.Console.WriteLine($"Number --> {num2} <-- is greater!");
+        System.Console.WriteLine($"Number --> {num1} <-- is smaller!");
+    }
+    else
+    {
+        System.Console.WriteLine($"Number --> {num1} = number {num2} <--");
+    }
+}
